c57977303289972b7b517cbd9c4cfc1a14908df1
Linco <Linco.nanco@gmail.com>
2019-06-26 20:03:40 -0400

    small fix for handling queries that return primitives (Select, Count, Any)

--- Dook/Query.cs
+++ Dook/Query.cs
@@ -22,7 +22,7 @@ namespace Dook
             }
             this.QueryProvider = provider;
             this.Predicate = Expression.Constant(this);
-            GetTableData();
+            if (typeof(T).GetInterfaces().Contains(typeof(IEntity))) GetTableData();
         }
 
         public Query(QueryProvider provider, Expression expression)
@@ -43,7 +43,7 @@ namespace Dook
             }
             this.QueryProvider = provider;
             this.Predicate = expression;
-            GetTableData();
+            if (typeof(T).GetInterfaces().Contains(typeof(IEntity))) GetTableData();
         }
 
         private void GetTableData()
