fdce223d7758f00d225b1520391de7b47f6d23a6
Linco <Linco.nanco@gmail.com>
2019-07-15 23:33:01 -0400

    modifying translation tests

--- Dook.Tests/MySqlTranslatorTests.cs
+++ Dook.Tests/MySqlTranslatorTests.cs
@@ -2,20 +2,33 @@ using System;
 using System.Linq;
 using Xunit;
 using Dook.Tests.Models;
+using System.Collections.Generic;
+using System.Linq.Expressions;
 
 namespace Dook.Tests
 {
     public class MySqlTranslatorTests
     {
-        [Fact]
-        public void TestQuery()
-        {
-            MySQLTranslator translator = new MySQLTranslator();
+        /// <summary>
+        /// Testing query translation
+        /// </summary>
+        /// <returns></returns>
+        public static IEnumerable<object[]> QueryTestsData()
+        {   
             Query<TestModel> queryObject = new Query<TestModel>(new QueryProvider(new DbProvider(DbType.Sql, "Server=127.0.0.1;Database=fakedb;User Id=FakeUser;Password=fake.password;")));
             IQueryable<TestModel> query = queryObject.Where(t => t.BoolProperty && t.EnumProperty == TestEnum.Three);
+            yield return new object[]
+            {
+                query,
+                "SELECT t.Id, t.BoolProperty, t.CreatedOn, t.DateTimeProperty, t.EnumProperty, t.StringProperty, t.UpdatedOn FROM TestModels AS t WHERE (t.BoolProperty AND (t.EnumProperty = @P0))"
+            };
+        }
+        [Theory, MemberData("QueryTestsData")]
+        public void QueryTests(IQueryable<TestModel> query, string expectedResult)
+        {
+            MySQLTranslator translator = new MySQLTranslator();
             SQLPredicate Sql = translator.Translate(query.Expression);
-            Assert.Equal("SELECT t.Id, t.BoolProperty, t.CreatedOn, t.DateTimeProperty, t.EnumProperty, t.StringProperty, t.UpdatedOn FROM TestModels AS t WHERE (t.BoolProperty AND (t.EnumProperty = @P0))", Sql.Sql);
-            Assert.Equal(Sql.Parameters["@P0"],2);
+            Assert.Equal(expectedResult, Sql.Sql);
         }  
     }
 }
--- Dook.Tests/SqlTranslatorTests.cs
+++ Dook.Tests/SqlTranslatorTests.cs
@@ -1,22 +1,33 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using Dook.Tests.Models;
 using Xunit;
 
-namespace Dook
+namespace Dook.Tests
 {
     public class SqlTranslatorTests
     {
-        [Fact]
-        public void TestQuery()
+        /// <summary>
+        /// Testing query translation
+        /// </summary>
+        /// <returns></returns>
+        public static IEnumerable<object[]> QueryTestsData()
+        {   
+            Query<TestModel> queryObject = new Query<TestModel>(new QueryProvider(new DbProvider(DbType.Sql, "Server=127.0.0.1;Database=fakedb;User Id=FakeUser;Password=fake.password;")));
+            IQueryable<TestModel> query = queryObject.Where(t => t.BoolProperty && t.EnumProperty == TestEnum.Three);
+            yield return new object[]
+            {
+                query,
+                "SELECT * FROM (SELECT t.[Id], t.[BoolProperty], t.[CreatedOn], t.[DateTimeProperty], t.[EnumProperty], t.[StringProperty], t.[UpdatedOn] FROM TestModels AS t) AS t WHERE (t.[BoolProperty] AND (t.[EnumProperty] = @P0))"
+            };
+        }
+        [Theory, MemberData("QueryTestsData")]
+        public void QueryTests(IQueryable<TestModel> query, string expectedResult)
         {
             SQLServerTranslator translator = new SQLServerTranslator();
-            Query<TestModel> queryObject = new Query<TestModel>(new QueryProvider(new DbProvider(DbType.Sql, "Server=127.0.0.1;Database=fakedb;User Id=FakeUser;Password=fake.password;")));
-            IQueryable<TestModel> query = queryObject.Where(t => t.BoolProperty == true && t.EnumProperty == TestEnum.Three);
             SQLPredicate Sql = translator.Translate(query.Expression);
-            Assert.Equal("SELECT * FROM (SELECT t.[Id], t.[BoolProperty], t.[CreatedOn], t.[DateTimeProperty], t.[EnumProperty], t.[StringProperty], t.[UpdatedOn] FROM TestModels AS t) AS t WHERE ((t.[BoolProperty] = @P0) AND (t.[EnumProperty] = @P1))", Sql.Sql);
-            Assert.Equal(Sql.Parameters["@P0"],true);
-            Assert.Equal(Sql.Parameters["@P1"],2);
+            Assert.Equal(expectedResult, Sql.Sql);
         }   
     }
 }
